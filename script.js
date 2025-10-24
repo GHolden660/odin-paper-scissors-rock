7bf176c0f27a392ca0bd6f2a7055614379335a51
Gina Holden <212469938+GHolden660@users.noreply.github.com>
2025-10-24 17:03:58 +1300

    Add playSingleRound function

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playSingleRound(humanChoice, computerChoice) {
+    if (humanChoice === "paper" && computerChoice == "rock"){
+        console.log("You win! Paper beats Rock!");
+        humanScore += 1;
+    } else if (humanChoice === "paper" && computerChoice == "paper"){
+        console.log("Draw!")
+    } else if (humanChoice === "paper" && computerChoice == "scissors"){
+        console.log("You lose! Scissors beats Paper!")
+        computerScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice == "paper"){
+        console.log("You win! Scissors beats Paper!");
+        humanScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice == "scissors"){
+        console.log("Draw!")
+    } else if (humanChoice === "scissors" && computerChoice == "rock"){
+        console.log("You lose! Rock beats Scissors!")
+        computerScore += 1;
+    } else if (humanChoice === "rock" && computerChoice == "scissors"){
+        console.log("You win! Rocks beats Scissors");
+        humanScore += 1;
+    } else if (humanChoice === "rock" && computerChoice == "rock"){
+        console.log("Draw!")
+    } else if (humanChoice === "rock" && computerChoice == "paper"){
+        console.log("You lose! Paper beats Rock!")
+        computerScore += 1;
+    }
+}
+
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+
+playSingleRound(humanChoice, computerChoice);
