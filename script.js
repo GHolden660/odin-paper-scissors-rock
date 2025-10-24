2d4c060ab979ff8ee80fa5e768af1d82e9b7fab2
Gina Holden <212469938+GHolden660@users.noreply.github.com>
2025-10-24 16:51:50 +1300

    Create computer and user selection functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world!")
+function getComputerChoice() {
+    num = Math.random();
+    if (num < 1/3){
+        return "paper";
+    } else if (num < 2/3){
+        return "scissors";
+    } else {
+        return "rock";
+    }
+}
+
+function getHumanChoice(){
+    choice = prompt("Make your choice! Paper, scissors, or rock?").toLowerCase();
+    return choice;
+}
